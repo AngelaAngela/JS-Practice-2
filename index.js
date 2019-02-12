5f398200d7ff9900c4ad2a1465d47d9a25de315c
angie <angieangerilli@gmail.com>
2019-02-12 18:28:28 -0500

    JS Exercise

---
+++ index.js
@@ -0,0 +1,5 @@
+let name = prompt("What is your name?");
+let company = prompt("What company do you work at?");
+let job = prompt("What is your job title?");
+
+console.log(`${name} works at ${company} as a ${job}`);
